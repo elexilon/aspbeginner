e558053d68d04783d443dfb00f7fb7d0484bf43f
elexilon <elexilon@gmail.com>
2018-04-02 23:28:54 +0200

    create edit and delete

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -12,12 +12,23 @@ namespace WebApplication1.Pages
     {
         private readonly AppDbContext _db;
         public IndexModel(AppDbContext db) { _db = db; }
-
-        [BindProperty]
         public IList<Customer> Customers { get; private set; }
         public async Task OnGetAsync()
         {
             Customers = await _db.Customers.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var customer = await _db.Customers.FindAsync(id);
+
+            if(customer != null)
+            {
+                _db.Customers.Remove(customer);
+                await _db.SaveChangesAsync();
+            }
+
+            return RedirectToPage();
         }
     }
 }
